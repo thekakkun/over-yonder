3417bab780cd1ee9cb71f9038aece68ff0b8d6bf
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2022-10-31 15:57:20 -0400

    Create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header({}) {
+  return (
+    <header className="w-full bg-slate-800 p-2">
+      <h1 className="text-slate-100 text-lg font-bold">Point!</h1>
+    </header>
+  );
+}
