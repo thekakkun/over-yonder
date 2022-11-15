9f8d9a1def9b783bf3c474fa0eb0772e69fad9b4
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2022-11-15 12:06:10 -0500

    Add zoom and pan functionality

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,6 +3,9 @@ import {
   geoAzimuthalEquidistant,
   geoPath,
 } from "d3-geo";
+import { select } from "d3-selection";
+import { zoom } from "d3-zoom";
+
 import { useEffect, useRef } from "react";
 import colors from "tailwindcss/colors";
 
@@ -31,61 +34,72 @@ export default function Map({ target, location }: MapProps) {
         geoJson as ExtendedFeatureCollection
       );
     }
-  }, [svgRef]);
+  }, [svgRef, projection]);
 
   const guessDest = getDestination(location, target.heading, 5000);
   const geoGenerator = geoPath(projection);
 
+  const globeData = {
+    fill: colors.blue[100],
+    d:
+      geoGenerator({
+        type: "Sphere",
+      }) ?? "",
+  };
+
+  const countriesData = (geoJson as ExtendedFeatureCollection).features.map(
+    (feature) => {
+      return {
+        stroke: colors.slate[800],
+        fill: colors.stone[100],
+        d: geoGenerator(feature) ?? "",
+      };
+    }
+  );
+
+  const destLineData = {
+    stroke: colors.red[500],
+    strokeWidth: 3,
+    fill: undefined,
+    d:
+      geoGenerator({
+        type: "LineString",
+        coordinates: [
+          [location.longitude, location.latitude],
+          [target.longitude, target.latitude],
+        ],
+      }) ?? "",
+  };
+  const guessLineData = {
+    stroke: colors.green[500],
+    strokeWidth: 3,
+    fill: undefined,
+    d:
+      geoGenerator({
+        type: "LineString",
+        coordinates: [
+          [location.longitude, location.latitude],
+          [guessDest.longitude, guessDest.latitude],
+        ],
+      }) ?? "",
+  };
+
+  function handleZoom(e: any) {
+    select("#map").selectChildren().attr("transform", e.transform);
+  }
+  useEffect(() => {
+    let zoomBehavior = zoom<SVGSVGElement, unknown>().on("zoom", handleZoom);
+    select<SVGSVGElement, unknown>("#map").call(zoomBehavior);
+  }, []);
+
   return (
-    <svg ref={svgRef} id="map" className="h-full">
-      <MapElement
-        svg={{
-          fill: colors.blue[100],
-          d:
-            geoGenerator({
-              type: "Sphere",
-            }) ?? "",
-        }}
-      ></MapElement>
-      <MapElement
-        svg={(geoJson as ExtendedFeatureCollection).features.map((feature) => {
-          return {
-            stroke: colors.slate[800],
-            fill: colors.stone[100],
-            d: geoGenerator(feature) ?? "",
-          };
-        })}
-      ></MapElement>
-      <MapElement
-        svg={{
-          stroke: colors.red[500],
-          strokeWidth: 3,
-          fill: undefined,
-          d:
-            geoGenerator({
-              type: "LineString",
-              coordinates: [
-                [location.longitude, location.latitude],
-                [target.longitude, target.latitude],
-              ],
-            }) ?? "",
-        }}
-      ></MapElement>
-      <MapElement
-        svg={{
-          stroke: colors.green[500],
-          strokeWidth: 3,
-          fill: undefined,
-          d:
-            geoGenerator({
-              type: "LineString",
-              coordinates: [
-                [location.longitude, location.latitude],
-                [guessDest.longitude, guessDest.latitude],
-              ],
-            }) ?? "",
-        }}
-      ></MapElement>
-    </svg>
+    <div className="h-full">
+      <svg ref={svgRef} id="map" className="h-full w-full">
+        <MapElement svg={globeData}></MapElement>
+        <MapElement svg={countriesData}></MapElement>
+        <MapElement svg={destLineData}></MapElement>
+        <MapElement svg={guessLineData}></MapElement>
+      </svg>
+    </div>
   );
 }
