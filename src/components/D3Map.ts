5266322472cdd0683ab7a7d1277b9c6fb8044997
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2022-11-17 16:13:39 -0500

    Implement versor dragging

--- src/components/D3Map.ts
+++ src/components/D3Map.ts
@@ -1,13 +1,17 @@
+// @ts-nocheck
+import { drag } from "d3-drag";
 import {
   GeoProjection,
   GeoPath,
   geoAzimuthalEquidistant,
   ExtendedFeatureCollection,
   geoPath,
+  geoOrthographic,
 } from "d3-geo";
-import { select, BaseType, Selection } from "d3-selection";
-import { ZoomBehavior, zoom, zoomIdentity } from "d3-zoom";
-import colors from "tailwindcss/colors";
+import { pointers, select, Selection } from "d3-selection";
+import { ZoomBehavior, zoom } from "d3-zoom";
+
+import versor from "versor";
 
 import geoJson from "../assets/data/ne_50m_admin_0_countries.json";
 import { Coordinates } from "../types/cartography";
@@ -20,6 +24,7 @@ import { getBearing, getDestination } from "../utilities/cartography";
 export default class D3Map {
   svg: Selection<SVGSVGElement, unknown, null, undefined>;
   target: CompletedLocation;
+  destination: Coordinates;
   location: Coordinates;
   projection: GeoProjection;
   geoGenerator: GeoPath;
@@ -39,8 +44,9 @@ export default class D3Map {
     this.svg = select(containerEl);
     this.target = target;
     this.location = location;
+    this.destination = getDestination(this.location, this.target.heading, 5000);
 
-    this.projection = geoAzimuthalEquidistant()
+    this.projection = geoOrthographic()
       .rotate([
         -this.location.longitude,
         -this.location.latitude,
@@ -51,105 +57,106 @@ export default class D3Map {
         geoJson as ExtendedFeatureCollection
       );
     this.geoGenerator = geoPath(this.projection);
-    this.zoomBehavior = zoom();
 
-    this.draw();
-    this.initZoom();
-    this.zoomToElem(select("#destLine"));
-  }
-
-  /** Attach zoom behavior on target svg. */
-  initZoom() {
-    this.zoomBehavior.on("zoom", (e: any) =>
-      this.svg.selectChildren().attr("transform", e.transform)
-    );
-    this.svg.call(this.zoomBehavior);
-  }
-
-  zoomToElem(element: Selection<BaseType, unknown, HTMLElement, any>) {
-    const containerEl = this.svg.node();
-    if (containerEl !== null) {
-      const { clientHeight, clientWidth } = containerEl;
-      const { height, width, x, y } = containerEl.getBBox();
-      this.svg.call(
-        this.zoomBehavior.transform,
-        zoomIdentity.translate(0, 0).scale(1)
-      );
-    }
+    this.drag = this.drag.bind(this);
+    this.svg
+      .call(
+        this.drag().on("drag.render", () => {
+          this.draw();
+        })
+      )
+      .call(() => this.draw());
   }
 
   /** Draw the map. */
   draw() {
-    this.svg.selectChildren().remove();
-    this.drawGlobe();
-    this.drawCountries();
-    this.drawDest();
-    this.drawGuess();
-  }
+    this.svg.select("#globe").attr(
+      "d",
+      this.geoGenerator({
+        type: "Sphere",
+      })
+    );
 
-  /** Draw the globe background. */
-  drawGlobe() {
-    this.svg
-      .append("path")
-      .attr("fill", colors.blue[100])
-      .attr(
-        "d",
-        this.geoGenerator({
-          type: "Sphere",
-        })
-      );
-  }
-
-  /** Draw the countries. */
-  drawCountries() {
-    this.svg
-      .append("g")
-      .attr("fill", colors.stone[100])
-      .attr("stroke", colors.slate[800])
+    const u = this.svg
+      .select("#countries")
       .selectAll("path")
-      .data((geoJson as ExtendedFeatureCollection).features)
-      .enter()
-      .append("path")
-      .attr("d", this.geoGenerator);
+      .data((geoJson as ExtendedFeatureCollection).features);
+    u.enter().append("path").merge(u).attr("d", this.geoGenerator);
+
+    this.svg.select("#destLine").attr(
+      "d",
+      this.geoGenerator({
+        type: "LineString",
+        coordinates: [
+          [this.location.longitude, this.location.latitude],
+          [this.target.longitude, this.target.latitude],
+        ],
+      })
+    );
+
+    this.svg.select("#guessLine").attr(
+      "d",
+      this.geoGenerator({
+        type: "LineString",
+        coordinates: [
+          [this.location.longitude, this.location.latitude],
+          [this.destination.longitude, this.destination.latitude],
+        ],
+      })
+    );
   }
 
-  /** Draw the destination line. */
-  drawDest() {
-    this.svg
-      .append("path")
-      .attr("id", "destLine")
-      .attr("fill", null)
-      .attr("stroke", colors.red[500])
-      .attr("stroke-width", "3px")
-      .attr(
-        "d",
-        this.geoGenerator({
-          type: "LineString",
-          coordinates: [
-            [this.location.longitude, this.location.latitude],
-            [this.target.longitude, this.target.latitude],
-          ],
-        })
-      );
-  }
+  drag() {
+    let v0, q0, r0, a0, l;
 
-  /** Draw the guess line */
-  drawGuess() {
-    const guessDest = getDestination(this.location, this.target.heading, 5000);
-    this.svg
-      .append("path")
-      .attr("fill-opacity", 0)
-      .attr("stroke", colors.green[500])
-      .attr("stroke-width", "3px")
-      .attr(
-        "d",
-        this.geoGenerator({
-          type: "LineString",
-          coordinates: [
-            [this.location.longitude, this.location.latitude],
-            [guessDest.longitude, guessDest.latitude],
-          ],
-        })
-      );
+    const pointer = (event) => {
+      const t = pointers(event, this.svg);
+
+      if (t.length !== l) {
+        l = t.length;
+        if (l > 1) a0 = Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]);
+        dragStarted(event);
+      }
+
+      // For multitouch, average positions and compute rotation.
+      if (l > 1) {
+        const x = t.reduce((acc, curr) => acc + curr[0], 0);
+        const y = t.reduce((acc, curr) => acc + curr[1], 0);
+
+        const a = Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]);
+        return [x, y, a];
+      }
+
+      return t[0];
+    };
+
+    const dragStarted = (event) => {
+      v0 = versor.cartesian(this.projection.invert(pointer(event)));
+      q0 = versor((r0 = this.projection.rotate()));
+    };
+
+    const dragged = (event) => {
+      const p = pointer(event, this);
+      const v1 = versor.cartesian(this.projection.rotate(r0).invert(p));
+      const delta = versor.delta(v0, v1);
+      let q1 = versor.multiply(q0, delta);
+
+      // For multitouch, compose with a rotation around the axis.
+      if (p[2]) {
+        const d = (p[2] - a0) / 2;
+        const s = -Math.sin(d);
+        const c = Math.sign(Math.cos(d));
+        q1 = versor.multiply([Math.sqrt(1 - s * s), 0, 0, c * s], q1);
+      }
+
+      this.projection.rotate(versor.rotation(q1));
+
+      // In vicinity of the antipode (unstable) of q0, restart.
+      if (delta[0] < 0.7) {
+        dragStarted(event);
+      }
+    };
+
+    return drag().on("start", dragStarted).on("drag", dragged);
   }
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import colors from "tailwindcss/colors";
 
 import { Coordinates } from "../types/cartography";
 import { CompletedLocation } from "../types/game";
@@ -19,11 +20,30 @@ export default function Map({ target, location }: MapProps) {
     if (svgRef.current) {
       map = new D3Map(svgRef.current, target, location);
     }
-  }, [svgRef, target]);
+  }, [svgRef]);
 
   return (
     <div className="h-full">
-      <svg ref={svgRef} id="map" className="h-full w-full"></svg>
+      <svg ref={svgRef} id="map" className="h-full w-full">
+        <path id="globe" fill={colors.blue[100]}></path>
+        <g
+          id="countries"
+          fill={colors.stone[100]}
+          stroke={colors.slate[800]}
+        ></g>
+        <path
+          id="destLine"
+          fillOpacity={0}
+          stroke={colors.red[500]}
+          strokeWidth="3px"
+        ></path>
+        <path
+          id="guessLine"
+          fillOpacity={0}
+          stroke={colors.green[500]}
+          strokeWidth="3px"
+        ></path>
+      </svg>
     </div>
   );
 }
